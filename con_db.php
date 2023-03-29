ad6adedd7dbfdbfab4ea223a4c3260e6e4155011
Juan-Terrazas-A <117481506+Juan-Terrazas-A@users.noreply.github.com>
2023-03-29 09:26:21 -0700

    Enlace a la base de datos

---
+++ con_db.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$database = "septimo_encuentro";
+$username = "root";
+$password = "Overcooked#1";
+// Create connection
+$conex = mysqli_connect($servername, $username, $password, $database);
+/*// Check connection
+if (!$conex) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conex);
+*/
+?>
